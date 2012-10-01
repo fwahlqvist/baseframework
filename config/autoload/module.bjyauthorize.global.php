74487c195269e8777d92662be57a43ea90783706
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2012-10-01 22:13:34 +0100

    Bjy route working

---
+++ config/autoload/module.bjyauthorize.global.php
@@ -0,0 +1,75 @@
+<?php
+
+return array(
+    'bjyauthorize' => array(
+        'default_role' => 'guest',
+        'identity_provider' => 'BjyAuthorize\Provider\Identity\ZfcUserZendDb',
+        'unauthorized_strategy' => 'BjyAuthorize\View\UnauthorizedStrategy',
+        
+        
+        
+        /* role providers simply provide a list of roles that should be inserted
+         * into the Zend\Acl instance. the module comes with two providers, one
+         * to specify roles in a config file and one to load roles using a
+         * Zend\Db adapter.
+         */
+        'role_providers' => array(
+
+            /* here, 'guest' and 'user are defined as top-level roles, with
+             * 'admin' inheriting from user
+             */
+            'BjyAuthorize\Provider\Role\Config' => array(
+                'guest' => array(),
+                'user'  => array('children' => array(
+                    'admin' => array(),
+                )),
+            ),
+
+            // this will load roles from the user_role table in a database
+            // format: user_role(role_id(varchar), parent(varchar))
+            'BjyAuthorize\Provider\Role\ZendDb' => array(
+                'table'             => 'user_role',
+                'role_id_field'     => 'role_id',
+                'parent_role_field' => 'parent',
+            ),
+        ),
+
+
+        
+        'resource_providers' => array(
+        ),
+        'rule_providers' => array(
+        ),
+'guards' => array(
+            /* If this guard is specified here (i.e. it is enabled), it will block
+             * access to all controllers and actions unless they are specified here.
+             * You may omit the 'action' index to allow access to the entire controller
+             */
+        /*    'BjyAuthorize\Guard\Controller' => array(
+                array('controller' => 'index', 'action' => 'index', 'roles' => array('guest','user')),
+                array('controller' => 'index', 'action' => 'stuff', 'roles' => array('user')),
+                array('controller' => 'zfcuser', 'roles' => array()),
+                // Below is the default index action used by the [ZendSkeletonApplication](https://github.com/zendframework/ZendSkeletonApplication)
+                // array('controller' => 'Application\Controller\Index', 'roles' => array('guest', 'user')),
+            ),
+*?
+            /* If this guard is specified here (i.e. it is enabled), it will block
+             * access to all routes unless they are specified here.
+             */
+            'BjyAuthorize\Guard\Route' => array(
+                array('route' => 'zfcuser', 'roles' => array('user')),
+                array('route' => 'zfcuser/logout', 'roles' => array('user')),
+                array('route' => 'zfcuser/login', 'roles' => array('guest')),
+                array('route' => 'zfcuser/register', 'roles' => array('guest')),
+                // Below is the default index action used by the [ZendSkeletonApplication](https://github.com/zendframework/ZendSkeletonApplication)
+                array('route' => 'home', 'roles' => array('guest', 'user')),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_map' => array(
+            'error/403' => __DIR__ . '/../view/error/403.phtml',
+        ),
+    ),
+);
