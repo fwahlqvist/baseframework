630c397ce798d3d3e107f94b46ef51308d08606e
Fredrik Wahlqvist <fwahlqvist@gmail.com>
2012-10-23 22:43:33 +0100

    ManuFrog merge

--- config/autoload/zetheme.config.global.php
+++ config/autoload/zetheme.config.global.php
@@ -1,16 +1,12 @@
 <?php
 return array(
     'ze_theme' => array(
-        'default_theme' => 'defualt',
+        'default_theme' => 'ZF2MobileTheme',
         'theme_paths' => array(
             __DIR__ . '/../../themes/'
         ),
-        'routes'=>array(
-        'defualt'=>array('album', 'blog')
-        ),
         'adapters' => array(
             'ZeTheme\Adapter\Configuration',
-            'ZeTheme\Adapter\Route',
         ),
     ),
 );
